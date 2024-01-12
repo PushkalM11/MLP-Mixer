1d298ec599267a23e2fb6c0b821a58dbdafa95a4
Pushkal Mishra <ee20btech11042@iith.ac.in>
2024-01-12 13:31:16 +0530

    Added files

--- Models/mlp_mixer.py
+++ Models/mlp_mixer.py
@@ -21,24 +21,24 @@ class MixerBlock(nn.Module):
 
     def __init__(self, dim, num_patch, token_dim, channel_dim, dropout = 0.):
         super().__init__()
-
-        self.token_mix = nn.Sequential(
+        
+        self.token_mixing = nn.Sequential(
             nn.LayerNorm(dim),
             Rearrange('b n d -> b d n'),
             FeedForward(num_patch, token_dim, dropout),
             Rearrange('b d n -> b n d')
         )
 
-        self.channel_mix = nn.Sequential(
+        self.channel_mixing = nn.Sequential(
             nn.LayerNorm(dim),
             FeedForward(dim, channel_dim, dropout),
         )
 
     def forward(self, x):
 
-        x = x + self.token_mix(x)
+        x = x + self.token_mixing(x)
 
-        x = x + self.channel_mix(x)
+        x = x + self.channel_mixing(x)
 
         return x
 
@@ -47,38 +47,40 @@ class MLPMixer(nn.Module):
 
     def __init__(self, in_channels, dim, num_classes, patch_size, image_size, depth, token_dim, channel_dim):
         super().__init__()
-
+        # dim is number of channels - C
+        # channel_dim is the hidden dimension of the channel MLP
         assert image_size % patch_size == 0, 'Image dimensions must be divisible by the patch size.'
-        self.num_patch =  (image_size// patch_size) ** 2
-        self.to_patch_embedding = nn.Sequential(
+        self.num_patch =  (image_size// patch_size) ** 2  #num_patch is number of tokens 
+        #token_dim is the hidden dimension of the token MLP
+        self.stem = nn.Sequential(
             nn.Conv2d(in_channels, dim, patch_size, patch_size),
             Rearrange('b c h w -> b (h w) c'),
         )
 
-        self.mixer_blocks = nn.ModuleList([])
+        self.MixerBlock = nn.ModuleList([])
 
         for _ in range(depth):
-            self.mixer_blocks.append(MixerBlock(dim, self.num_patch, token_dim, channel_dim))
+            self.MixerBlock.append(MixerBlock(dim, self.num_patch, token_dim, channel_dim))
 
-        self.layer_norm = nn.LayerNorm(dim)
+        self.pre_head_layer_norm = nn.LayerNorm(dim)
 
-        self.mlp_head = nn.Sequential(
+        self.head = nn.Sequential(
             nn.Linear(dim, num_classes)
         )
 
     def forward(self, x):
 
 
-        x = self.to_patch_embedding(x)
+        x = self.stem(x)
 
-        for mixer_block in self.mixer_blocks:
+        for mixer_block in self.MixerBlock:
             x = mixer_block(x)
 
-        x = self.layer_norm(x)
+        x = self.pre_head_layer_norm(x)
 
         x = x.mean(dim=1)
 
-        return self.mlp_head(x)
+        return self.head(x)
 
 
 
@@ -96,7 +98,3 @@ if __name__ == "__main__":
     out_img = model(img)
 
     print("Shape of out :", out_img.shape)  # [B, in_channels, image_size, image_size]
-
-
-
-
